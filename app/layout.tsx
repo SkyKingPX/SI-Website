1f437b7976a7b7b929172c03f9850380b5c87f96
SkyKing_PX <skykingpx@emberforge.dev>
2025-07-14 19:59:26 +0200

    Make Light Mode default

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
 import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import type {Metadata} from "next"
+import {Inter} from "next/font/google"
 import "./global.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import {ThemeProvider} from "@/components/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata: Metadata = {
-  title: "EmberForge Development",
-  description: "The official EmberForge Development Website",
+    title: "Soncresity Industries",
+    description: "The official Website of Soncresity Industries",
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
-          {children}
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+            {children}
         </ThemeProvider>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
